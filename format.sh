d3fa4a432393231b9dd74c173b5e95c32a7aa1f0
Jonathan Merritt <j.s.merritt@gmail.com>
2021-12-19 15:50:47 +1100

    Formatting

--- format.sh
+++ format.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
-find . -name '*.hs' -exec bash -xc 'brittany --write-mode=inplace {}' \;
+readonly brittany=$(command -v brittany)
+readonly cabalfmt=$(command -v cabal-fmt)
+
+if [ -z "${brittany}" ]; then
+    echo "Could not find brittany executable!"
+fi
+if [ -z "${cabalfmt}" ]; then
+    echo "Could not find cabal-fmt executable!"
+fi
+
+echo "Using brittany:  ${brittany}"
+echo "Using cabal-fmt: ${cabalfmt}"
+
+find . -name '*.hs' -not -path '*/dist-newstyle/*' -exec \
+    bash -xc "${brittany} --write-mode=inplace {}" \;
+find . -name '*.cabal' -not -path '*/dist-newstyle/*' -exec \
+    bash -xc "${cabalfmt} --inplace {}" \;
